25b6812da211da442a5ab0060ff25aa3d6e907dc
RyanSolanki <ryansolanki1@gmail.com>
2023-04-27 14:34:47 -0700

    Added new file for misplaced tile heuristic

---
+++ misplaced_tile.py
@@ -0,0 +1,47 @@
+#uniform cost search
+from puzzle import Puzzle
+
+def uniform_cost_search():
+    print("TODO: implement me!")
+
+def Misplaced_Tile_Heuristic(currentPuzzle, goalPuzzle):
+    sum = 0
+    for i in range(0, len(currentPuzzle)):
+        if currentPuzzle[i] != goalPuzzle[i] and currentPuzzle[i] != 'b':
+            sum = sum + 1
+    return sum
+        # for j in range(0, len(currentPuzzle[i])):
+        #     if currentPuzzle[i][j] != goalPuzzle[i][j]:
+        #         return 1
+
+def Astar():
+    print("TODO: implement me!")
+
+def main():
+
+    print("Welcome to 862326974 8 puzzle solver.")
+    puzzleSize = int(input("Enter the puzzle size (3 for 8-puzzle, 4 for 15-puzzle, etc.)\n"))
+    puzzleType = input("Type “1” to use a default puzzle, or “2” to enter your own puzzle.\n")
+    
+    p = Puzzle([0]*(pow(puzzleSize,2)), puzzleSize)
+    #p.print_puzzle()
+    
+    if puzzleType == "1":
+        p.update_state(['1', '2', '3', '4', 'b', '5', '6', '7', '8'])
+        p.print_puzzle()
+    elif puzzleType == "2":
+        print("Enter your puzzle, use a zero to represent the blank")
+        sNums = []
+        for i in range(puzzleSize):
+            currentRow = input("Enter the values for row " + str(i + 1) + " separated by spaces\n")
+            currentNumString = currentRow.split(" ")
+            for string in currentNumString:
+                sNums.append(string)
+        p.update_state(sNums)
+    
+    p.print_puzzle()
+    
+    Astar()
+
+if __name__ == "__main__":
+    main()
